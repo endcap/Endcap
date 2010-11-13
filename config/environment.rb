374e55cc5d70405c70936c5cb1db8b9f8785f13f
Jon Evans <jon@plantwars.com>
2010-11-13 14:08:39 -0600

    More Sass

--- config/environment.rb
+++ config/environment.rb
@@ -2,7 +2,7 @@
 require File.expand_path('../application', __FILE__)
 
 #Sass::Plugin.options[:template_location] = "#{Rails.root}/app/styles"
-Sass::Plugin.options[:template_location] = { 'app/stylesheets' => 'public/stylesheets' }
+Sass::Plugin.options[:template_location] = { 'app/styles' => 'public/stylesheets' }
 
 # Initialize the rails application
 Endcap::Application.initialize!
