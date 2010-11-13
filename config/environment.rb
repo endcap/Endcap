ecb6fb52dafd27a39e199f4a24912de97cf91fb8
Jon Evans <jon@plantwars.com>
2010-11-13 14:02:19 -0600

    More SASS fixes

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,8 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
-Sass::Plugin.options[:template_location] = "#{Rails.root}/app/styles"
+#Sass::Plugin.options[:template_location] = "#{Rails.root}/app/styles"
+Sass::Plugin.options[:template_location] = { 'app/stylesheets' => 'public/stylesheets' }
 
 # Initialize the rails application
 Endcap::Application.initialize!
