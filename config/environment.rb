b7a60ec01fc54c654b8af1c4c00d66ad4efbbcf5
Jon Evans <jon@plantwars.com>
2010-11-13 15:53:01 -0600

    Restored SASS, removed css. Adding band list to profile

--- config/environment.rb
+++ config/environment.rb
@@ -1,8 +1,8 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
-#Sass::Plugin.options[:template_location] = "#{Rails.root}/app/styles"
-Sass::Plugin.options[:template_location] = { 'app/styles' => 'public/stylesheets' }
+Sass::Plugin.options[:template_location] = "#{Rails.root}/app/styles"
+#Sass::Plugin.options[:template_location] = { 'app/styles' => 'public/stylesheets' }
 
 # Initialize the rails application
 Endcap::Application.initialize!
