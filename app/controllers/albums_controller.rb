fe5dc01ceaa73fdcb4cd2aa17e28c6e5c8cbc4ef
Jon Evans <jon@artfuldodgersoftware.com>
2010-11-14 11:15:31 -0600

    Fixed problem with editing events. Added drop down band selector to albums page. Made pages redirect back to themselves after save.

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -43,6 +43,16 @@ class AlbumsController < ApplicationController
   # POST /albums
   # POST /albums.xml
   def create
+    new_band = params[:album][:band_id]
+    this_venue = String.new
+    
+    if new_band.length > 0
+      added_band = Band.new
+      added_band = Band.find_by_id(new_band)
+      this_band = added_band
+      params[:album][:band] = this_band
+    end
+    
     @album = Album.new(params[:album])
 
     respond_to do |format|
@@ -62,6 +72,18 @@ class AlbumsController < ApplicationController
     @album = Album.find(params[:id])
 
     respond_to do |format|
+      
+      new_band = params[:album][:band_id]
+
+      if new_band.length > 0
+        added_band = Band.new
+        added_band = Band.find_by_id(new_band)
+        
+        @album.band = added_band
+        
+      end
+      params[:album][:band] = @album.band
+      
       if @album.update_attributes(params[:album])
         format.html { redirect_to(@album, :notice => 'Album was successfully updated.') }
         format.xml  { head :ok }
