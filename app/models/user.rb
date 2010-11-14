2494bd00bb3ed7655e49561da9c2e4b5dad7483e
Jon Evans <jon@plantwars.com>
2010-11-14 16:12:19 -0600

    Ability to get events and albums a user is associated with

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,38 @@ class User < ActiveRecord::Base
   def location
     [self.city, self.state].join(", ")
   end
+  
+  def events
+    eventArray = Array.new
+    
+    self.bands.each do |band|
+      band.events.each do |event|
+        if !eventArray.include? event
+          eventArray << event
+        end
+        logger.info "added event named #{event.name}"
+      end
+    end
+    
+    eventArray
+  end
+  
+  def albums(number=2)
+    # call with number of albums you want, but we start counting at 0
+    if number > 0
+      number = number-1
+    end
+    albumArray = Array.new
+    
+    self.bands.each do |band|
+      band.albums.each do |album|
+        if !albumArray.include? album
+          albumArray << album
+        end
+      end
+    end
+    
+    albumArray[0..number]
+  end
+  
 end
