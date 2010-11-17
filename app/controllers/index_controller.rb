690d53fd54d6f2748671c89a4d11b73f973a84d2
Jon Evans <jon@plantwars.com>
2010-11-16 23:09:18 -0600

    Display list of bands, events, and songs on index page from database

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -3,7 +3,9 @@ class IndexController < ApplicationController
   def index
     @bands = Band.last(5)
     @users = User.last(5)
+    @events = Event.last(5)
     @venues = Venue.last(5)
+    @songs = Song.last(5)
 
     respond_to do |format|
       format.html # index.html.erb
