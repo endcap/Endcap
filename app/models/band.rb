6604532e47c71ac1aa21c4f0e12720f8ce2a78a9
Jon Evans <jon@plantwars.com>
2011-03-02 21:30:58 -0600

    Makes songs belong to bands as well as albums

--- app/models/band.rb
+++ app/models/band.rb
@@ -4,6 +4,7 @@ class Band < ActiveRecord::Base
   has_many :band_events
   has_many :events, :through => :band_events
   has_many :albums
+  has_many :songs
   
   validates :name, :presence => true
   
