58b0bc16a4bd2a24e3c361612b87739eb512dd51
Jon Evans <jon@plantwars.com>
2011-08-02 23:29:41 -0600

    Add Sass to Gemfile and move .scss files to default location.

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,6 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
-Sass::Plugin.options[:template_location] = "#{Rails.root}/app/styles"
 #Sass::Plugin.options[:template_location] = { 'app/styles' => 'public/stylesheets' }
 
 # Initialize the rails application
